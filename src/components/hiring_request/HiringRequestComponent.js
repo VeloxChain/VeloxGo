c81b77e2ff25c3cb22faaff70fa55a5d9d5dd2f8
Ai Bui <ai.bui@enablestartup.com>
2018-06-21 14:51:51 +0700

    [update UI] update page Hiring Request

--- src/components/hiring_request/HiringRequestComponent.js
+++ src/components/hiring_request/HiringRequestComponent.js
@@ -1,110 +1,73 @@
 import React, { Component } from "react";
 import styles from "./HiringRequestComponentStyle";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 import TextField from "material-ui/TextField";
-import {Tabs, Tab} from "material-ui/Tabs";
-import Toggle from "material-ui/Toggle";
+import Bike from "./BikeComponent";
+import _ from "lodash";
 
 class HiringRequestComponent extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            value: "a",
+            value: 1,
+
+            bikeData: [
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" },
+                { image: "images/bike.png" }
+            ]
         };
     }
 
-    handleChange = (value) => {
-        this.setState({
-            value: value,
+    handleChange = (event, index, value) => this.setState({value});
+
+    _renderBike = (bike) => {
+        let renderBike = [];
+        
+        _.forEach(bike, (value, index) => {
+            renderBike.push(<Bike bike={value} key={index} />);
         });
-    };
+
+        return renderBike;
+    }
 
     render() {
         return (
             <div style={styles.wrapp}>
+                <div className="row" style={styles.mb20}>
+                    <div className="col-sm-3">
+                        <SelectField
+                            floatingLabelText="Location"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        >
+                            <MenuItem value={1} primaryText="Singapore" />
+                            <MenuItem value={2} primaryText="United States" />
+                            <MenuItem value={3} primaryText="Viet Nam" />
+                        </SelectField>
+                    </div>
+                    <div className="col-sm-9">
+                        <div style={styles.map}>
+                            <TextField
+                                floatingLabelText="Search"
+                                fullWidth
+                            />
+                            <div style={styles.divIcon}>
+                                <i className="fa fa-map-o" style={styles.icon} />
+                                <i className="fa fa-map-marker" style={styles.icon} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="row">
-                    <div className="col-sm-7">
-                        <div style={styles.wrappLeft}>
-                            <TextField
-                                floatingLabelText="Owner"
-                                fullWidth
-                            />
-                            <TextField
-                                floatingLabelText="Manufacturer"
-                                fullWidth
-                            />
-                            <TextField
-                                floatingLabelText="Bike Serial"
-                                fullWidth
-                            />
-                            <TextField
-                                floatingLabelText="Bike Wallet Address"
-                                fullWidth
-                            />
-
-                            <Tabs
-                                value={this.state.value}
-                                onChange={this.handleChange}
-                                tabItemContainerStyle={styles.tabItemContainerStyle}
-                                style={styles.tabs}
-                            >
-                                <Tab label="Location" value="a" style={styles.tab}>
-                                    <div style={styles.location}>
-                                        <div className="row">
-                                            <div className="col-sm-8">
-                                                <TextField
-                                                    floatingLabelText="Address"
-                                                    fullWidth
-                                                />
-                                            </div>
-                                            <div className="col-sm-4">
-                                                <button style={styles.buttonSave}>Save</button>
-                                            </div>
-                                        </div>
-                                        <iframe
-                                            title={"googlemaps"}
-                                            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15348.641730062593!2d108.35025619999999!3d15.9006638!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1529391376898"
-                                            style={{border:0, width: "100%", height: "300px", marginTop: 20}}
-                                        />
-                                    </div>
-                                </Tab>
-                                <Tab label="Bike specifications" value="b" style={styles.tab}>
-                                    <div>
-                                        <h2>Bike specifications</h2>
-                                    </div>
-                                </Tab>
-                                <Tab label="Owner history" value="c" style={styles.tab}>
-                                    <div>
-                                        <h2>Owner history</h2>
-                                    </div>
-                                </Tab>
-                                <Tab label="Riding perfomance" value="d" style={styles.tab}>
-                                    <div>
-                                        <h2>Riding perfomance</h2>
-                                    </div>
-                                </Tab>
-                            </Tabs>
-
-                        </div>
-                    </div>
-                    <div className="col-sm-5">
-                        <div style={styles.wrappRight}>
-                            <div style={styles.boxLeft}>
-                                <img src="images/bike.png" alt="Bikecoin" style={styles.bike} />
-                            </div>
-                            <div style={styles.boxRight}>
-                                <div>
-                                    <button style={styles.buttonTransfer}>Transfer</button>
-                                    <button style={styles.buttonDelete}>Delete</button>
-                                    <Toggle
-                                        label="UNLOCKED"
-                                        labelPosition="right"
-                                        style={styles.toggle}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {this._renderBike(this.state.bikeData)}
                 </div>
             </div>
         );
