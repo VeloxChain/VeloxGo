33e04bf164943bbcadb321fb1667529df6265791
layvd <lay.vdi@gmail.com>
2018-06-27 15:59:35 +0700

    [update] add map for edit page

--- src/components/your_bikes/edit_bike/bike_info/BikeLocations.js
+++ src/components/your_bikes/edit_bike/bike_info/BikeLocations.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import styles from "../EditBikeComponentStyle";
+import MapEditBike from '../../../map_edit_bike/MapEditBike'
 class BikeLocation extends Component {
     render() {
         return (
             <div style={styles.location}>
-                <div className="row">
+                {/* <div className="row">
                     <div className="col-sm-12">
                         <TextField
                             floatingLabelText="Address"
@@ -14,12 +15,8 @@ class BikeLocation extends Component {
                             onChange={(e) => this.props.handleChangeState({location: e.target.value})}
                         />
                     </div>
-                </div>
-                <iframe
-                    title={"googlemaps"}
-                    src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15348.641730062593!2d108.35025619999999!3d15.9006638!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1529391376898"
-                    style={{border:0, width: "100%", height: "300px", marginTop: 20}}
-                />
+                </div> */}
+                <MapEditBike {...this.props}/>
             </div>
         );
     }
