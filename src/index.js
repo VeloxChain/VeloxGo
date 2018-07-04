6aade20230937a8457cadced32752a079a07b9dc
layvd <lay.vdi@gmail.com>
2018-07-04 17:22:20 +0700

    [update] update build and update router

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import { BrowserRouter } from "react-router-dom";
 // import rootReducer from './reducers'
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import store from "./store";
+
+import createHistory from 'history/createHashHistory';
+
 // import ethereum from './services/ethereum';
 // const createStoreWithMiddleware = applyMiddleware()(createStore)
 
@@ -25,7 +28,7 @@ import store from "./store";
 ReactDOM.render(
     <Provider store={store}>
         <MuiThemeProvider>
-            <BrowserRouter>
+            <BrowserRouter history={createHistory()}>
                 <App />
             </BrowserRouter>
         </MuiThemeProvider>
