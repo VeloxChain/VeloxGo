31bd36ab0bc1d580c6577f11f5977fcc101fe0dc
Ai Bui <ai.bui@enablestartup.com>
2018-10-02 15:57:39 +0700

    fix search google maps

--- src/components/map_register_bike/MapRegisterBike.js
+++ src/components/map_register_bike/MapRegisterBike.js
@@ -13,7 +13,7 @@ const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox
 
 const MapRegisterBike = compose(
     withProps({
-        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${appConfig.google_map_api_key}&v=3.exp&libraries=geometry,drawing,places`,
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${appConfig.google_map_api_key}&v=3.31&libraries=geometry,drawing,places`,
         loadingElement: <div style={{ height: "100%" }} />,
         containerElement: <div style={{ height: "400px" }} />,
         mapElement: <div style={{ height: "100%" }} />,
